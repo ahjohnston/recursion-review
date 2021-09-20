d9d02db037544c34c05bb413c3e76b88ca848fff
Annie Johnston <annie89@gmail.com>
2021-09-20 14:34:25 -0400

    finished base cases (primitive data types)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,20 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //base case: input parameter is a number or string or boolean
+  ////
+  if(typeof obj === 'boolean'){
+    return String(obj);
+  }
+  if(typeof obj === 'number'){
+    return String(obj);
+  }
+  if(typeof obj === 'string'){
+    return '"' + (obj) + '"'
+  }
+  if(obj === null){
+    return String(null);
+  }
+
+  //recursive case: input is array or object
 };
